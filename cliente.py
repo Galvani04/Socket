5f25a308208426a1b52019f47ce9cd8214eff554
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2020-11-12 01:46:57 -0300

    adicao de comentarios e melhora de resposta

--- cliente.py
+++ cliente.py
@@ -1,18 +1,26 @@
 import socket
+
+# gera o socket
+# AF_INET == ipv4
+# SOCK_STREAM == TCP
+
 HOST = '127.0.0.1'
 PORT = 50000
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+s.connect((socket.gethostname(), PORT))
+
 while True:
     data = input()
     s.sendall(bytes(data,"utf-8"))
     data = s.recv(1024)
-    
-    if not data:
-        print('Fechando a conexão!')
+    msg = data.decode("utf-8")
+
+    if (msg == "fechar") or (not data):
+        s.sendall(bytes("fechar","utf-8"))
         break
     else:
-        msg = data.decode("utf-8")
         print(msg)
 
+print('Fechando a conexão!')
 s.close()
