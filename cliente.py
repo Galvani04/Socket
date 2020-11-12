db45c4b6ba46adefffee19ef1ad6134da07f78c7
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2020-11-12 14:54:18 -0300

    Adição de comentarios explicando o funcionamento do codigo

--- cliente.py
+++ cliente.py
@@ -7,20 +7,24 @@ import socket
 HOST = '127.0.0.1'
 PORT = 50000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), PORT))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Gera um socket ipv4 com protocolo tcp
+s.connect((socket.gethostname(), PORT)) # Se conecta ou host e a porta
 
+# Mantem a interatividade ate que a conexao se encerre
 while True:
-    data = input()
-    s.sendall(bytes(data,"utf-8"))
-    data = s.recv(1024)
-    msg = data.decode("utf-8")
+    data = input() # Libera a entrada de dados no console
+    s.sendall(bytes(data,"utf-8")) # Envia a mensagem para o servidor
+    data = s.recv(1024) # Gera um buffer de 1024 bytes e espera mensagem
+    msg = data.decode("utf-8") # Decodifica a mendagem para utf-8
 
+    # Se receber "fechar" ou um dado vazio finaliza a conexao
     if (msg == "fechar") or (not data):
+        # Pede para o servidor encerrar a conexao
         s.sendall(bytes("fechar","utf-8"))
-        break
+        break # Finaliza o loop
     else:
-        print(msg)
+        print(msg) # printa a mensagem enviada pelo servidor
 
 print('Fechando a conexão!')
+# Encerra a conexao do lado cliente
 s.close()
--- servidor.py
+++ servidor.py
@@ -7,25 +7,31 @@ import socket
 HOST = ''
 PORT = 50000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), PORT))
-s.listen(1)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Gera um socket ipv4 com protocolo tcp
+s.bind((socket.gethostname(), PORT)) # Se vincula a um host name e port
+s.listen(1) # Gera uma quantidade x de posiveis conexões com o servidor
 
 print('Aguardando conexão')
-(conn, ender) = s.accept()
+(conn, ender) = s.accept() # Aguarda a requisição do cliente
 
-print(f'conectado em {ender}')
+print(f'conectado em {ender}') # Formata a string e mostra quem se conectou
 while True:
+    # Gera um buffer de 1024 bytes e espera mensagem
     data = conn.recv(1024)
-    msg = data.decode("utf-8")
+
+    # Transforma os dados recebidos de bytes para utf-8
+    msg = data.decode("utf-8")                              
     
+    # Se a mensagem for "fechar" encerra a conexao
     if "fechar" == msg:
         print('Fechando a conexão')
+        # Pede para o cliente encerrar a conexao
         conn.sendall(bytes('',"utf-8"))
-        break
+        break # Finaliza o loop
     else:
-        print(msg)
-        data = input()
-        conn.sendall(bytes(data,"utf-8"))
+        print(msg) # printa a mensagem enviada pelo cliente
+        data = input() # Libera a entrada de dados no console
+        conn.sendall(bytes(data,"utf-8")) # Envia o dado digitado pelo cliente
 
+# Encerra a conexao do lado servidor
 conn.close()
