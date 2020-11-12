7f6cf21c3157fc8f72c45cc3323f3270eb48b8d7
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2020-11-11 23:08:28 -0300

    Melhora na emissão e adicao de recepcao de dados do servidor

--- cliente.py
+++ cliente.py
@@ -3,6 +3,16 @@ HOST = '127.0.0.1'
 PORT = 50000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall(str.encode('Boa noite matheus equilatero'))
-data = s.recv(1024)
-print('Mensagem recebida', data.decode())
+while True:
+    data = input()
+    s.sendall(bytes(data,"utf-8"))
+    data = s.recv(1024)
+    
+    if not data:
+        print('Fechando a conexão!')
+        break
+    else:
+        msg = data.decode("utf-8")
+        print(msg)
+
+s.close()
